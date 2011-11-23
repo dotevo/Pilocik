f6cb096d6cc00d4a07dde54020b84f6a7ceb9f94
elsejwier <tc112233@gmail.com>
2011-11-23 15:46:47 +0100

    partition save to binary file (start)

--- lib/PiLibocik/main.cpp
+++ lib/PiLibocik/main.cpp
@@ -6,24 +6,24 @@
 
 int main(){
 
-        {
-    //Tworzenie przyk³adowych danych
-    QList <PiLibocik::Partition::Node> nodes;
-    PiLibocik::Partition::Way w(1,0.5,(quint8)0);
-    //Dodaj 10node do drogi 1
-    for(int i=0;i<10;i++){
-        PiLibocik::Partition::Node n(i,0.1*i+0.1,0.1*i+0.1);
-        PiLibocik::Partition::Edge e1( (i+1)%10,0.1 );
-        //PiLibocik::Partition::Edge e2
-        n.addBoundaryEdge(e1);
-        n.addWay(1);
-        nodes.append(n);
-        w.addNode(i);
-    }
-    PiLibocik::Partition::PartitionFile p("test","car",QIODevice::WriteOnly,1);
-    QList <PiLibocik::Partition::Way> ways;
-    ways.append(w);
-    p.savePartition(ways,nodes,2);
+    {
+        //Tworzenie przyk³adowych danych
+        QList <PiLibocik::Partition::Node> nodes;
+        PiLibocik::Partition::Way w(1,0.5,(quint8)0);
+        //Dodaj 10node do drogi 1
+        for(int i=0;i<10;i++){
+            PiLibocik::Partition::Node n(i,0.1*i+0.1,0.1*i+0.1);
+            PiLibocik::Partition::Edge e1( (i+1)%10,0.1 );
+            //PiLibocik::Partition::Edge e2
+            n.addBoundaryEdge(e1);
+            n.addWay(1);
+            nodes.append(n);
+            w.addNode(i);
+        }
+        PiLibocik::Partition::PartitionFile p("test","car",QIODevice::WriteOnly,1);
+        QList <PiLibocik::Partition::Way> ways;
+        ways.append(w);
+        p.savePartition(ways,nodes,2);
     }
 
     PiLibocik::Partition::PartitionFile p("test","car",QIODevice::ReadOnly,1);
