dff432c22b61a58ca9b90e1bbce96107b5563a41
Adam Jordanek <dotevo@gmail.com>
2011-12-01 12:08:15 +0100

    PluginListWidget

--- Pilocik/pluginswindow.cpp
+++ Pilocik/pluginswindow.cpp
@@ -1,6 +1,70 @@
 #include "pluginswindow.h"
 #include "ui_pluginswindow.h"
 
+
+PluginListWidget::PluginListWidget(QWidget* parent)
+                : QListWidget(parent){
+    setSelectionMode(QAbstractItemView::ContiguousSelection);
+    for (int i = 0; i < 10; ++i){
+        QListWidgetItem* item = new QListWidgetItem(QString::number(i+i));
+        item->setCheckState(Qt::Unchecked);
+        addItem(item);
+    }
+}
+
+void PluginListWidget::mousePressEvent(QMouseEvent* event){
+    QListWidgetItem* item = selectedCheckStateItem(event->pos());
+    if (item)
+    {
+        // mouse pressed over a selected items checkbox,
+        // change the check states of all selected items
+        setSelectedCheckStates(item->checkState() == Qt::Checked ? Qt::Unchecked : Qt::Checked);
+    }
+    else
+    {
+        // otherwise let the base class handle mouse press
+        QListWidget::mousePressEvent(event);
+    }
+}
+
+
+void PluginListWidget::mouseReleaseEvent(QMouseEvent* event){
+    // do nothing if mouse released over a selected items checkbox
+    if (!selectedCheckStateItem(event->pos()))
+    {
+        // otherwise let the base class handle mouse release
+        QListWidget::mouseReleaseEvent(event);
+    }
+}
+
+QListWidgetItem* PluginListWidget::selectedCheckStateItem(const QPoint& pos){
+    QListWidgetItem* item = itemAt(pos);
+    if (item)
+    {
+        // with the help of styles, check if checkbox rect contains 'pos'
+        QStyleOptionButton opt;
+        opt.QStyleOption::operator=(viewOptions());
+        opt.rect = visualItemRect(item);
+        QRect r = style()->subElementRect(QStyle::SE_ViewItemCheckIndicator, &opt);
+        // assure that the item is also selected
+        if (selectedItems().contains(item) && r.contains(pos))
+        {
+            return item;
+        }
+    }
+    return 0;
+}
+
+void PluginListWidget::setSelectedCheckStates(Qt::CheckState checkState){
+    foreach (QListWidgetItem* item, selectedItems())
+        item->setCheckState(checkState);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 PluginsWindow::PluginsWindow(NavigationWindow *parent) :
     QFullScreenFrame(parent),
     ui(new Ui::PluginsWindow)
@@ -18,3 +82,6 @@ void PluginsWindow::on_okButton_clicked(){
     setVisible(false);
     emit closed();
 }
+
+
+
--- Pilocik/pluginswindow.h
+++ Pilocik/pluginswindow.h
@@ -2,6 +2,13 @@
 #define PLUGINSWINDOW_H
 
 #include "qfullscreenframe.h"
+#include <QApplication>
+#include <QListWidget>
+#include <QListWidgetItem>
+#include <QMouseEvent>
+#include <QStyle>
+#include <QStyleOptionViewItem>
+#include <QStyleOptionButton>
 
 
 class NavigationWindow;
@@ -9,6 +16,26 @@ class NavigationWindow;
 namespace Ui {
     class PluginsWindow;
 }
+
+
+class PluginListWidget : public QListWidget
+{
+public:
+    PluginListWidget(QWidget* parent = 0);
+
+protected:
+    void mousePressEvent(QMouseEvent* event);
+    void mouseReleaseEvent(QMouseEvent* event);
+
+private:
+    // returns the selected item whose checkbox lies under 'pos'
+    // returns 0 if not selected, no item at pos, or does not fit inside checkbox
+    QListWidgetItem* selectedCheckStateItem(const QPoint& pos);
+    // sets the check state of all selected items to 'checkState'
+    void setSelectedCheckStates(Qt::CheckState checkState);
+};
+
+
 
 class PluginsWindow : public QFullScreenFrame
 {
